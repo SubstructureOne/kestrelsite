8b9085ca76458ae5e2cfe3a9b6ee7c41dacf592b
dek <dek@substructure.one>
2022-09-25 17:29:34 -0400

    Update favicon

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -30,8 +30,12 @@ export const Headers: FunctionComponent<HeadersProps> = ({title}) => {
         <link href="/css/normalize.css" rel="stylesheet" type="text/css" />
         <link href="/css/webflow.css" rel="stylesheet" type="text/css" />
         <link href="/css/kestrel.webflow.css" rel="stylesheet" type="text/css" />
-        <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-        <link href="/images/webclip.png" rel="apple-touch-icon" />
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="black"/>
     </>
 }
