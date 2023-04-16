834c1dd4bee268eb0647d763f0faa4dffc581a12
dek <dek@substructure.one>
2023-04-16 10:19:20 -0400

    Get the stripe integration working

--- src/pages/api/billing/hooks.ts
+++ src/pages/api/billing/hooks.ts
@@ -3,15 +3,22 @@
 import {NextApiRequest, NextApiResponse} from "next"
 import stripe from "../../../utils/stripe"
 import Stripe from "stripe"
+import getRawBody from "raw-body"
+
+export const config = {
+    api: {
+        bodyParser: false,
+    },
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    const payload = req.body
-    const sig_header = req.headers["STRIPE_SIGNATURE"]
+    const requestBuffer = await getRawBody(req, {limit: '1mb'});
+    const sig_header = req.headers["stripe-signature"]
     if (sig_header === undefined) {
-        res.status(401).json({error: "Missing STRIPE_SIGNATURE header"})
+        res.status(401).json({error: "Missing stripe-signature header"})
         return
     } else if (Array.isArray(sig_header)) {
-        res.status(401).json({error: "Multiple STRIPE_SIGNATURE headers specified"})
+        res.status(401).json({error: "Multiple stripe-signature headers specified"})
         return
     }
     if (process.env.STRIPE_WEBHOOK_SECRET === undefined) {
@@ -19,11 +26,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return
     }
     const event = stripe.webhooks.constructEvent(
-        payload, sig_header, process.env.STRIPE_WEBHOOK_SECRET
+        requestBuffer, sig_header, process.env.STRIPE_WEBHOOK_SECRET
     ) as Stripe.DiscriminatedEvent
 
     if (event.type == "invoice.payment_succeeded") {
         const invoice = event.data
         console.log(`Found an invoice; amount paid is ${invoice.object.amount_paid}`)
     }
+    res.status(200).json({message: "Success"})
 }
