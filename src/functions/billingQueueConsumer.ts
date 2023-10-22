90741d2186fc658be2e2eeaf0dd79cb35f6011d9
dek <dek@substructure.one>
2023-10-22 00:19:09 -0400

    Add password field

--- src/functions/billingQueueConsumer.ts
+++ src/functions/billingQueueConsumer.ts
@@ -6,6 +6,7 @@ import words from "friendly-words"
 
 import PasswordGenerator from "generate-password";
 import {encryptDataWithKey} from "../utils/encrypt_server";
+import {KResult, Ok, Err} from "../utils/errors";
 import logger from "../utils/logger";
 
 
@@ -35,32 +36,36 @@ export async function billingEventHandler(txn: NewExternalTransactionInfo) {
 }
 
 async function saveTransaction(client: Client, transaction: NewExternalTransactionInfo) {
-    const userInfo = await getuser(client, transaction.user_id)
+    const userInfo = await getuser(client, transaction.user_id);
     if (userInfo === null) {
         // user has not yet been provisioned
-        await provisionUser(client, transaction.user_id)
+        await provisionUser(client, transaction.user_id);
     }
-    await createExternalTransaction(client, transaction)
+    await createExternalTransaction(client, transaction);
 }
 
 
-async function provisionUser(client: Client, userId: string): Promise<AccountInfo> {
+async function provisionUser(client: Client, userId: string): Promise<KResult<AccountInfo>> {
     const pgName = generateUsername();
     const pgPasswordEnc = generateEncryptedPassword();
-    logger.info(`Provisioning user ${userId} with postgres username ${pgName}`)
-    const newUser: NewUserInfo = {user_id: userId, pg_name: pgName, pg_password_enc: pgPasswordEnc}
-    return await createUser(client, newUser)
+    logger.info(`Provisioning user ${userId} with postgres username ${pgName}`);
+    if (pgPasswordEnc.isErr) {
+        logger.error("Couldn't provision user", {userId});
+        return Err({friendly: "Couldn't provision user", cause: pgPasswordEnc});
+    }
+    const newUser: NewUserInfo = {user_id: userId, pg_name: pgName, pg_password_enc: pgPasswordEnc.value};
+    return Ok(await createUser(client, newUser));
 }
 
 
 function generateUsername() {
-    const { predicates, objects } = words
-    const predicate = predicates[Math.floor(Math.random() * predicates.length)]
-    const object = objects[Math.floor(Math.random() * objects.length)]
-    return `${predicate}${object}`
+    const { predicates, objects } = words;
+    const predicate = predicates[Math.floor(Math.random() * predicates.length)];
+    const object = objects[Math.floor(Math.random() * objects.length)];
+    return `${predicate}${object}`;
 }
 
-function generateEncryptedPassword(): Uint8Array {
+function generateEncryptedPassword(): KResult<Uint8Array> {
     const password = PasswordGenerator.generate({
         length: 16,
         numbers: true,
@@ -70,7 +75,9 @@ function generateEncryptedPassword(): Uint8Array {
         exclude: " ",
     });
     const pgpassKeyB64 = process.env.PGPASS_KEY_B64;
-    const binaryKey = Buffer.from(pgpassKeyB64 || "", "base64");
-    btoa
-    return encryptDataWithKey(password, binaryKey);
+    if (pgpassKeyB64 === undefined) {
+        return Err({friendly: "Postgres encryption key not defined"})
+    }
+    const binaryKey = Buffer.from(pgpassKeyB64, "base64");
+    return Ok(encryptDataWithKey(password, binaryKey));
 }
