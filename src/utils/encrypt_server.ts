492fa4a3cc051a3c6ae979ccb5fe9bf881904cb6
dek <dek@substructure.one>
2023-10-25 08:55:50 -0400

    Use auth tag in encryption

--- src/utils/encrypt_server.ts
+++ src/utils/encrypt_server.ts
@@ -6,17 +6,18 @@ export function encryptDataWithKey(secretData: string, key: Uint8Array): Uint8Ar
     const algorithm = 'aes-256-gcm';
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encrypted = cipher.update(secretData, 'utf8');
-    encrypted = Buffer.concat([iv, encrypted, cipher.final()]);
-    return new Uint8Array(encrypted);
+    const encrypted = Buffer.concat([cipher.update(secretData, 'utf8'), cipher.final()]);
+    const authTag = cipher.getAuthTag();
+    return Uint8Array.from(Buffer.concat([iv, authTag, encrypted]));
 }
 
 export function decryptDataWithKey(encrypted: Uint8Array, key: Uint8Array): string {
     const algorithm = 'aes-256-gcm';
     const iv = encrypted.slice(0, 16);
-    const encData = encrypted.slice(16);
+    const authTag = encrypted.slice(16, 32);
+    const encData = encrypted.slice(32);
     const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decrypted = decipher.update(encData);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    decipher.setAuthTag(authTag);
+    const decrypted = Buffer.concat([decipher.update(encData), decipher.final()]);
     return new TextDecoder().decode(decrypted);
 }
