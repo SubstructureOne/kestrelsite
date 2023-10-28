566b827446ebcee74b5230718786e340388c68c9
dek <dek@substructure.one>
2023-10-28 09:20:55 -0400

    Catch managed PG connection errors

--- src/utils/managed_db.ts
+++ src/utils/managed_db.ts
@@ -34,7 +34,13 @@ export async function managed_pgconnect(): Promise<KResult<Client>> {
         password,
         database: user,
     });
-    await client.connect();
+    try {
+        await client.connect();
+    } catch (error) {
+        const message = "Connection to managed PG failed";
+        logger.error(message);
+        return Err({friendly: message});
+    }
     return Ok(client);
 }
 
